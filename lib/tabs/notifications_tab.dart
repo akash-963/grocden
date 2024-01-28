3564650132664db55b42e6002b66b1bfdfeba9dd
akash-963 <jareakash963@gmail.com>
2024-01-28 08:42:15 +0530

    Notifications Tab Update

--- lib/tabs/notifications_tab.dart
+++ lib/tabs/notifications_tab.dart
@@ -1,28 +1,3 @@
-// import 'package:flutter/material.dart';
-//
-// class NotificationsTab extends StatelessWidget {
-//   const NotificationsTab({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Text('Notifications Tab'),
-//     );
-//   }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
 import 'package:flutter/material.dart';
 
 import '../models/notification_model.dart';
@@ -32,39 +7,56 @@ class NotificationsTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<NotificationModel>>(
-      future: NotificationService().fetchNotifications(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator(); // Loading indicator while fetching data
-        }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notifications"),
 
-        if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        }
+      ),
+      body: FutureBuilder<List<NotificationModel>>(
+        future: NotificationService().fetchNotifications(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator()); // Loading indicator while fetching data
+          }
 
-        List<NotificationModel> notifications = snapshot.data ?? [];
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
 
-        if (notifications.isEmpty) {
-          return Center(child: Text('No notifications found.'));
-        }
+          List<NotificationModel> notifications = snapshot.data ?? [];
 
-        return ListView.builder(
-          itemCount: notifications.length,
-          itemBuilder: (context, index) {
-            final notification = notifications[index];
+          if (notifications.isEmpty) {
+            return Center(child: Text('No notifications found.'));
+          }
 
-            return ListTile(
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage(notification.imageUrl ?? ''),
-              ),
-              title: Text(notification.title),
-              subtitle: Text(notification.body),
-              // You can customize the UI based on your notification model
-            );
-          },
-        );
-      },
+          return ListView.separated(
+            separatorBuilder: (BuildContext context, int index) {
+              return SizedBox(height: 3);
+            },
+            padding: EdgeInsets.all(4),
+            shrinkWrap: true,
+            itemCount: notifications.length,
+            itemBuilder: (context, index) {
+              final notification = notifications[index];
+
+              return Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Color(0xff9fe2bf),
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(notification.imageUrl ?? ''),
+                  ),
+                  title: Text(notification.title),
+                  subtitle: Text(notification.body),
+                  // You can customize the UI based on your notification model
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
